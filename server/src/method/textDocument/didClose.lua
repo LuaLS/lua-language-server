9d2265692897472cb1efc3891ef89fe3a58126b0
最萌小汐 <sumneko@hotmail.com>
2018-11-23 17:02:29 +0800

    加个日志查bug

--- server/src/method/textDocument/didClose.lua
+++ server/src/method/textDocument/didClose.lua
@@ -1,5 +1,6 @@
 return function (lsp, params)
     local doc = params.textDocument
+    log.debug('Close file:', doc.uri)
     lsp:removeText(doc.uri, doc.version)
     return true
 end
--- server/src/method/textDocument/didOpen.lua
+++ server/src/method/textDocument/didOpen.lua
@@ -1,5 +1,6 @@
 return function (lsp, params)
     local doc = params.textDocument
+    log.debug('Open file:', doc.uri)
     lsp:saveText(doc.uri, doc.version, doc.text)
     return true
 end

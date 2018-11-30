50701d2c557c61870bafcc46ef35e191a23ad573
最萌小汐 <sumneko@hotmail.com>
2018-11-30 15:04:33 +0800

    支持多返回值

--- server/src/method/textDocument/definition.lua
+++ server/src/method/textDocument/definition.lua
@@ -10,33 +10,33 @@ return function (lsp, params)
     end
     -- lua是从1开始的，因此都要+1
     local position = lines:position(params.position.line + 1, params.position.character + 1)
-    local suc, start, finish = matcher.definition(ast, position, 'utf8')
+    local suc, results = matcher.definition(ast, position, 'utf8')
     if not suc then
-        if finish then
-            log.debug(start, uri)
-            finish.lua = nil
-            log.debug(table.dump(finish))
-        end
         return {}
     end
 
-    local start_row,  start_col  = lines:rowcol(start,  'utf8')
-    local finish_row, finish_col = lines:rowcol(finish, 'utf8')
+    local locations = {}
+    for i, result in ipairs(results) do
+        local start, finish = result[1], result[2]
+        local start_row,  start_col  = lines:rowcol(start,  'utf8')
+        local finish_row, finish_col = lines:rowcol(finish, 'utf8')
+        locations[i] = {
+            uri = uri,
+            range = {
+                start = {
+                    line = start_row - 1,
+                    character = start_col - 1,
+                },
+                ['end'] = {
+                    line = finish_row - 1,
+                    -- 这里不用-1，因为前端期待的是匹配完成后的位置
+                    character = finish_col,
+                },
+            }
+        }
+    end
 
-    local response = {
-        uri = uri,
-        range = {
-            start = {
-                line = start_row - 1,
-                character = start_col - 1,
-            },
-            ['end'] = {
-                line = finish_row - 1,
-                -- 这里不用-1，因为前端期待的是匹配完成后的位置
-                character = finish_col,
-            },
-        },
-    }
+    local response = locations
     local passed_clock = os.clock() - start_clock
     if passed_clock >= 0.01 then
         log.warn(('[转到定义]耗时[%.3f]秒，文件大小[%s]字节'):format(passed_clock, #lines.buf))

1d3b6f8c0194c5da4f80793da1828c034c4daeef
最萌小汐 <sumneko@hotmail.com>
2018-12-03 10:44:32 +0800

    编译模块

---
+++ server/compile.lua
@@ -0,0 +1,28 @@
+local fs = require 'bee.filesystem'
+local root = fs.current_path()
+
+local function compileRelease()
+    local msvc = require 'msvc'
+    if not msvc:initialize(141, 'utf8') then
+        error('Cannot found Visual Studio Toolset.')
+    end
+
+    local property = {
+        Configuration = 'Release',
+        Platform = 'x86'
+    }
+    msvc:compile('build', root / 'bee.lua' / 'project' / 'bee.sln', property)
+end
+
+local function copyFile()
+    local source = root / 'bee.lua' / 'bin' / 'x86' / 'Release'
+    local target = root / 'bin'
+    for _, name in ipairs {'bee.dll', 'lua.exe', 'lua54.dll'} do
+        fs.copy_file(source / name, target / name, true)
+    end
+end
+
+compileRelease()
+copyFile()
+
+print 'make complete.'

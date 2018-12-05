2d19737aab53701661a41933e12df08edf078bc1
最萌小汐 <sumneko@hotmail.com>
2018-12-05 17:34:12 +0800

    整理代码

--- server/src/matcher/definition.lua
+++ server/src/matcher/definition.lua
@@ -1,41 +1,4 @@
-
-local function isContainPos(obj, pos)
-    return obj.start <= pos and obj.finish + 1 >= pos
-end
-
-local function findResult(results, pos)
-    for _, var in ipairs(results.vars) do
-        for _, info in ipairs(var) do
-            if isContainPos(info.source, pos) then
-                return {
-                    type = 'var',
-                    var = var,
-                }
-            end
-        end
-    end
-    for _, dots in ipairs(results.dots) do
-        for _, info in ipairs(dots) do
-            if isContainPos(info.source, pos) then
-                return {
-                    type = 'dots',
-                    dots = dots,
-                }
-            end
-        end
-    end
-    for _, label in ipairs(results.labels) do
-        for _, info in ipairs(label) do
-            if isContainPos(info.source, pos) then
-                return {
-                    type = 'label',
-                    label = label,
-                }
-            end
-        end
-    end
-    return nil
-end
+local findResult = require 'matcher.find_result'
 
 local function tryMeta(var)
     local keys = {}

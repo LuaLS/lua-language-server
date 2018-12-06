91b0bd8f72687f94342f384a916638fc87f1fabb
最萌小汐 <sumneko@hotmail.com>
2018-12-06 13:59:19 +0800

    未定义的全局变量（之后需要排除内置全局变量）

--- server/src/matcher/diagnostics.lua
+++ server/src/matcher/diagnostics.lua
@@ -29,6 +29,30 @@ local function searchUnusedLocals(results, callback)
     end
 end
 
+local function searchUndefinedGlobal(results, callback)
+    for _, var in ipairs(results.vars) do
+        if var.type ~= 'field' then
+            goto NEXT_VAR
+        end
+        if  var.parent.key ~= '_ENV'
+        and var.parent.key ~= '_G'
+        then
+            goto NEXT_VAR
+        end
+        for _, info in ipairs(var) do
+            if info.type == 'set' then
+                goto NEXT_VAR
+            end
+        end
+        for _, info in ipairs(var) do
+            if info.type == 'get' then
+                callback(info.source.start, info.source.finish, var.key)
+            end
+        end
+        ::NEXT_VAR::
+    end
+end
+
 return function (ast, results, lines)
     local datas = {}
     -- 搜索未使用的局部变量
@@ -39,6 +63,16 @@ return function (ast, results, lines)
             level   = 'Warning',
             code    = code,
             message = 'Unused local', -- LOCALE
+        }
+    end)
+    -- 搜索读取未定义全局变量
+    searchUndefinedGlobal(results, function (start, finish, code)
+        datas[#datas+1] = {
+            start   = start,
+            finish  = finish,
+            level   = 'Warning',
+            code    = code,
+            message = 'Undefined global', -- LOCALE
         }
     end)
     return datas

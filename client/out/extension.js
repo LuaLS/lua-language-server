19e4bb38b32cadab634e4afac05b2a2cc89bc4a2
最萌小汐 <sumneko@hotmail.com>
2018-12-13 12:29:29 +0800

    把编译后的文件也提交上去

---
+++ client/out/extension.js
@@ -0,0 +1,63 @@
+"use strict";
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const path = require("path");
+const vscode_1 = require("vscode");
+const vscode_languageclient_1 = require("vscode-languageclient");
+let client;
+function activate(context) {
+    let language = vscode_1.env.language;
+    // If the extension is launched in debug mode then the debug server options are used
+    // Otherwise the run options are used
+    let serverOptions = {
+        run: {
+            command: context.asAbsolutePath(path.join('server', 'bin', 'lua.exe')),
+            args: [
+                '-E',
+                '-e',
+                'LANG="' + language + '"',
+                context.asAbsolutePath(path.join('server', 'main.lua'))
+            ],
+            options: {
+                cwd: context.asAbsolutePath(path.join('server')),
+            }
+        },
+        debug: {
+            command: context.asAbsolutePath(path.join('server', 'bin', 'lua.exe')),
+            args: [
+                '-E',
+                '-e',
+                'LANG="' + language + '"',
+                context.asAbsolutePath(path.join('server', 'main.lua'))
+            ],
+            options: {
+                cwd: context.asAbsolutePath(path.join('server')),
+            }
+        }
+    };
+    // Options to control the language client
+    let clientOptions = {
+        // Register the server for plain text documents
+        documentSelector: [{ scheme: 'file', language: 'lua' }],
+        synchronize: {
+            // Notify the server about file changes to '.clientrc files contained in the workspace
+            fileEvents: vscode_1.workspace.createFileSystemWatcher('**/.clientrc')
+        }
+    };
+    // Create the language client and start the client.
+    client = new vscode_languageclient_1.LanguageClient('sumneko.lua-lsp', 'sumneko.lua-lsp', serverOptions, clientOptions);
+    // Start the client. This will also launch the server
+    client.start();
+}
+exports.activate = activate;
+function deactivate() {
+    if (!client) {
+        return undefined;
+    }
+    return client.stop();
+}
+exports.deactivate = deactivate;
+//# sourceMappingURL=extension.js.map

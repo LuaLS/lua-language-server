86a9b83d68da99f26a56ce914c65698a3714f0da
最萌小汐 <sumneko@hotmail.com>
2018-12-26 14:12:57 +0800

    跨文件转到时，只找最后一个定义

--- server/src/matcher/implementation.lua
+++ server/src/matcher/implementation.lua
@@ -2,7 +2,7 @@ local function parseResultAcrossUri(positions, vm, result)
     -- 跨越文件时，遍历的是值的绑定信息
     for _, info in ipairs(result.value) do
         if info.type == 'set' and info.source.uri == result.value.uri then
-            positions[#positions+1] = {
+            positions[1] = {
                 info.source.start,
                 info.source.finish,
                 info.source.uri,
@@ -12,13 +12,19 @@ local function parseResultAcrossUri(positions, vm, result)
     if #positions == 0 then
         for _, info in ipairs(result.value) do
             if info.type == 'return' and info.source.uri == result.value.uri then
-                positions[#positions+1] = {
+                positions[1] = {
                     info.source.start,
                     info.source.finish,
                     info.source.uri,
                 }
             end
         end
+    end
+    if #positions == 0 then
+        positions[1] = {
+            0, 0,
+            result.value.uri,
+        }
     end
 end
 

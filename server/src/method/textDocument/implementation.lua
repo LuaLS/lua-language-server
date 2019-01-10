eb7c7b6dcae752b03332f4f6d9004381eb87d457
最萌小汐 <sumneko@hotmail.com>
2019-01-10 09:16:59 +0800

    判个空

--- server/src/method/textDocument/implementation.lua
+++ server/src/method/textDocument/implementation.lua
@@ -37,22 +37,24 @@ return function (lsp, params)
     for i, position in ipairs(positions) do
         local start, finish, valueUri = position[1], position[2], (position[3] or uri)
         local _, valueLines = lsp:loadVM(valueUri)
-        local start_row,  start_col  = valueLines:rowcol(start)
-        local finish_row, finish_col = valueLines:rowcol(finish)
-        locations[i] = {
-            uri =  valueUri,
-            range = {
-                start = {
-                    line = start_row - 1,
-                    character = start_col - 1,
-                },
-                ['end'] = {
-                    line = finish_row - 1,
-                    -- 这里不用-1，因为前端期待的是匹配完成后的位置
-                    character = finish_col,
-                },
+        if valueLines then
+            local start_row,  start_col  = valueLines:rowcol(start)
+            local finish_row, finish_col = valueLines:rowcol(finish)
+            locations[i] = {
+                uri =  valueUri,
+                range = {
+                    start = {
+                        line = start_row - 1,
+                        character = start_col - 1,
+                    },
+                    ['end'] = {
+                        line = finish_row - 1,
+                        -- 这里不用-1，因为前端期待的是匹配完成后的位置
+                        character = finish_col,
+                    },
+                }
             }
-        }
+        end
     end
 
     local response = locations

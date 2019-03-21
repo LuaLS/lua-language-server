9323ee02699e8f12a7ce19df873e69256b3a2678
最萌小汐 <sumneko@hotmail.com>
2019-03-21 14:40:27 +0800

    优先取call内部的参数

--- server/src/core/find_source.lua
+++ server/src/core/find_source.lua
@@ -20,8 +20,19 @@ local function findAtPos(sources, pos, level)
         return nil
     end
     table.sort(res, function (a, b)
+        if a == b then
+            return false
+        end
         local rangeA = a.finish - a.start
         local rangeB = b.finish - b.start
+        -- 特殊处理：func 'str' 的情况下，list与string的范围会完全相同，此时取string
+        if rangeA == rangeB then
+            if b.type == 'call' and #b == 1 and b[1] == a then
+                return true
+            else
+                return false
+            end
+        end
         return rangeA < rangeB
     end)
     local source = res[level or 1]

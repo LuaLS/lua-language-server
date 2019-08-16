61dcb4bfb8d6def480f80e9678af1adc8970e02d
最萌小汐 <sumneko@hotmail.com>
2019-08-16 17:20:32 +0800

    修正一个报错

--- server/src/uri.lua
+++ server/src/uri.lua
@@ -8,6 +8,9 @@ local OS = platform.OS == 'Windows' and 'win32' or 'unix'
 
 local function decode(uri)
     local obj = URI:new(uri)
+    if not obj.filesystem_path then
+        return ''
+    end
     local fullPath = obj:filesystem_path(OS)
     local path = fs.path(fullPath)
     return path

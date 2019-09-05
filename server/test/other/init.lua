2dd41281796f3a504b5b5e8192bb109e1ffaf178
最萌小汐 <sumneko@hotmail.com>
2019-09-05 11:32:48 +0800

    不用得了

--- server/test/other/init.lua
+++ server/test/other/init.lua
@@ -4,6 +4,8 @@ local path = fs.path '/a/b/c/d/e/../../../..'
 local absolute = fs.absolute(path)
 if platform.OS == 'Windows' then
     assert(absolute:string():sub(-2) == '/a', absolute:string())
-else
+elseif platform.OS == 'Linux' then
     assert(absolute:string():sub(-3) == '/a/', absolute:string())
+elseif platform.OS == 'macOS' then
+    -- 不支持
 end

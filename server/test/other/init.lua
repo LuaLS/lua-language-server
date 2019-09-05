9ae3e55dbf679bca37d6ddd51cdd13f547b71f1c
最萌小汐 <sumneko@hotmail.com>
2019-09-05 10:58:05 +0800

    康康

--- server/test/other/init.lua
+++ server/test/other/init.lua
@@ -3,7 +3,7 @@ local platform = require 'bee.platform'
 local path = fs.path '/a/b/c/d/e/../../../..'
 local absolute = fs.absolute(path)
 if platform.OS == 'Windows' then
-    assert(absolute:string():sub(-2) == '/a')
+    assert(absolute:string():sub(-2) == '/a', absolute:string())
 else
-    assert(absolute:string():sub(-3) == '/a/')
+    assert(absolute:string():sub(-3) == '/a/', absolute:string())
 end

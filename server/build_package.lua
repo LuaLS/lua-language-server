8d48ad6e7a887266447cdca264f239d4697ec810
最萌小汐 <sumneko@hotmail.com>
2019-09-05 21:34:45 +0800

    0.12.5

--- server/build_package.lua
+++ server/build_package.lua
@@ -1,7 +1,7 @@
 local json = require 'json'
 local diagDefault = require 'constant.DiagnosticDefaultSeverity'
 
-local VERSION = "0.12.4"
+local VERSION = "0.12.5"
 
 local package = {
     name = "lua",

535dfc3f9de2064a76608915aa85fe2486fb51bb
最萌小汐 <sumneko@hotmail.com>
2019-11-27 15:07:41 +0800

    修改make

--- make/install.lua
+++ make/install.lua
@@ -9,19 +9,20 @@ local bindir = CWD / 'build' / platform / 'bin'
 local exe = platform == 'msvc' and ".exe" or ""
 local dll = platform == 'msvc' and ".dll" or ".so"
 
-fs.create_directories(output)
-fs.copy_file(bindir / 'lni'..dll, output / 'lni'..dll, true)
-fs.copy_file(bindir / 'lpeglabel'..dll, output / 'lpeglabel'..dll, true)
-fs.copy_file(bindir / 'bee'..dll, output / 'bee'..dll, true)
-fs.copy_file(bindir / 'lua'..exe, output / 'lua-language-server'..exe, true)
-
 local bootstrap = CWD / "3rd" / "bee.lua" / "bootstrap"
 fs.copy_file(bootstrap / "main.lua",      CWD / 'build' / "main.lua",   true)
 fs.copy_file(bindir / ('bee'..dll),       CWD / 'build' / ('bee'..dll), true)
 fs.copy_file(bindir / ('bootstrap'..exe), CWD / 'build' / ('bootstrap'..exe), true)
+fs.copy_file(bindir / 'lua'..exe,         CWD / 'build' / ('lua'..exe), true)
 if platform == 'msvc' then
     fs.copy_file(bindir / 'lua54.dll',    CWD / 'build' / 'lua54.dll',  true)
 end
+
+fs.create_directories(output)
+fs.copy_file(bindir / 'lni'..dll,       output / 'lni'..dll, true)
+fs.copy_file(bindir / 'lpeglabel'..dll, output / 'lpeglabel'..dll, true)
+fs.copy_file(bindir / 'bee'..dll,       output / 'bee'..dll, true)
+fs.copy_file(bindir / 'lua'..exe,       output / 'lua-language-server'..exe, true)
 
 if platform == 'msvc' then
     fs.copy_file(bindir / 'lua54'..dll, output / 'lua54'..dll, true)

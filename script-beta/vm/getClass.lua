6b4ac4345b65bdf0a26f5f59feaa8353df3a10b1
最萌小汐 <sumneko@hotmail.com>
2020-01-23 14:34:36 +0800

    table中的class

--- script-beta/vm/getClass.lua
+++ script-beta/vm/getClass.lua
@@ -16,12 +16,31 @@ local function getClass(source, classes, deep)
             end
         end
     end)
+    if source.value then
+        vm.eachField(source.value, function (src)
+            local key = vm.getKeyName(src)
+            local lkey = key:lower()
+            if lkey == 's|type'
+            or lkey == 's|__name'
+            or lkey == 's|name'
+            or lkey == 's|class' then
+                if src.value and src.value.type == 'string' then
+                    classes[#classes+1] = src.value[1]
+                end
+            end
+        end)
+    end
     if #classes ~= 0 then
         return
     end
     vm.eachMeta(source, function (mt)
         getClass(mt, classes, deep + 1)
     end)
+    if source.value then
+        vm.eachMeta(source.value, function (mt)
+            getClass(mt, classes, deep + 1)
+        end)
+    end
 end
 
 function vm.getClass(source)

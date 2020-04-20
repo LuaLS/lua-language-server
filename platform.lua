d48e7f52fbfa43862bd21c0de008b9f6dd08dfb9
actboy168 <actboy168@gmail.com>
2020-04-20 10:54:18 +0800

    修改几个小问题

--- platform.lua
+++ platform.lua
@@ -12,13 +12,16 @@ local exePath = findExePath()
 local exeDir  = exePath:match('(.+)[/\\][%w_.-]+$')
 local dll     = package.cpath:match '[/\\]%?%.([a-z]+)'
 package.cpath = ('%s/?.%s'):format(exeDir, dll)
-if not package.loadlib(exeDir..'/bee.'..dll, 'luaopen_bee_platform') then
+local ok, err = package.loadlib(exeDir..'/bee.'..dll, 'luaopen_bee_platform')
+if not ok then
     error(([[It doesn't seem to support your OS, please build it in your OS, see https://github.com/sumneko/vscode-lua/wiki/Build
-exePath: %s
-exeDir:  %s
-dll:     %s
-cpath:   %s
+errorMsg: %s
+exePath:  %s
+exeDir:   %s
+dll:      %s
+cpath:    %s
 ]]):format(
+    err,
     exePath,
     exeDir,
     dll,

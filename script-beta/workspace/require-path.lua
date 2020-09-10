426b30ed25c597554169474ff26543b8ac500652
最萌小汐 <sumneko@hotmail.com>
2020-09-10 16:43:07 +0800

    暂存

--- script-beta/workspace/require-path.lua
+++ script-beta/workspace/require-path.lua
@@ -1,7 +1,38 @@
 local m = {}
 
---- `aaa/bbb/ccc.lua` 与 `?.lua` 将返回 `a.b.c`
-function m.getVisiblePath(path, searcher)
+m.cache = {}
+
+--- `aaa/bbb/ccc.lua` 与 `?.lua` 将返回 `aaa.bbb.cccc`
+local function getOnePath(path, searcher)
+    local stemPath     = path
+                        : gsub('%.[^%.]+$', '')
+                        : gsub('[/\\]+', '.')
+    local stemSearcher = searcher
+                        : gsub('%.[^%.]+$', '')
+                        : gsub('[/\\]+', '.')
+    local start        = stemSearcher:match '()%?' or 1
+    for pos = start, #stemPath do
+        local word = stemPath:sub(start, pos)
+        local newSearcher = stemSearcher:gsub('%?', word)
+        if newSearcher == stemPath then
+            return word
+        end
+    end
+    return nil
+end
+
+function m.getVisiblePath(path, searchers)
+    if not m.cache[path] then
+        local result = {}
+        m.cache[path] = result
+        for _, searcher in ipairs(searchers) do
+        end
+    end
+    return m.cache[path]
+end
+
+function m.flush()
+    m.cache = {}
 end
 
 return m

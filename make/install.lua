f7b60b523bfe92ae73776ce12e40a579a22738a9
最萌小汐 <sumneko@hotmail.com>
2021-03-02 10:26:11 +0800

    fix compile

--- make/install.lua
+++ make/install.lua
@@ -13,3 +13,8 @@ fs.create_directories(output)
 fs.copy_file(bindir / 'lpeglabel'..dll, output / 'lpeglabel'..dll, true)
 fs.copy_file(bindir / 'bee'..dll,       output / 'bee'..dll, true)
 fs.copy_file(bindir / 'lua'..exe,       output / 'lua-language-server'..exe, true)
+
+if platform == 'msvc' then
+    fs.copy_file(bindir / 'lua54'..dll, output / 'lua54'..dll, true)
+    require 'msvc'.copy_crtdll('x64', output)
+end

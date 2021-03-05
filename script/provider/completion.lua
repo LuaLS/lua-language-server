41b72b3c9915bbda4d7ae0e456d45791cc76b5ee
最萌小汐 <sumneko@hotmail.com>
2021-03-05 16:42:51 +0800

    close #419 VSCode has released new version

--- script/provider/completion.lua
+++ script/provider/completion.lua
@@ -4,7 +4,7 @@ local isEnable = false
 
 local function allWords()
     local str = [[abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.:('"[,#*@|=-{ ]]
-    local list = {'\t'}
+    local list = {'\t', '\n'}
     for c in str:gmatch '.' do
         list[#list+1] = c
     end

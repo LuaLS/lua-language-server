7f42574b769149fd0acad0f6396c933b278acc5b
最萌小汐 <sumneko@hotmail.com>
2021-08-04 16:53:05 +0800

    cleanup

--- script/core/searcher.lua
+++ script/core/searcher.lua
@@ -272,7 +272,7 @@ local function crossSearch(status, uri, expect, mode, sourceUri)
 end
 
 local function checkCache(status, uri, expect, mode)
-    local cache = vm.getCache('search:' .. mode)
+    local cache = status.cache
     local fileCache = cache[uri]
     if not fileCache then
         fileCache = {}

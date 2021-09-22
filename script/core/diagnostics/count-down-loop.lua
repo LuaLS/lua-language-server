9c79a00b41e1eff3a04a3daea6762ece89bb60eb
最萌小汐 <sumneko@hotmail.com>
2021-09-22 19:39:41 +0800

    fix

--- script/core/diagnostics/count-down-loop.lua
+++ script/core/diagnostics/count-down-loop.lua
@@ -10,30 +10,39 @@ return function (uri, callback)
     end
 
     guide.eachSourceType(state.ast, 'loop', function (source)
-        if not source.loc or not source.loc.value then
-            return
-        end
         local maxNumer = source.max and tonumber(source.max[1])
         if maxNumer ~= 1 then
             return
         end
-        local minNumber = source.loc and source.loc.value and tonumber(source.loc.value[1])
+        local minNumber = source.init and tonumber(source.init[1])
         if minNumber and minNumber <= 1 then
             return
         end
         if not source.step then
             callback {
-                start  = source.loc.value.start,
+                start  = source.init.start,
                 finish = source.max.finish,
-                message = lang.script('DIAG_COUNT_DOWN_LOOP', ('%s, %s'):format(text:sub(source.loc.value.start, source.max.finish), '-1'))
+                message = lang.script('DIAG_COUNT_DOWN_LOOP'
+                    , ('%s, %s'):format(text:sub(
+                        guide.positionToOffset(state, source.init.start),
+                        guide.positionToOffset(state, source.max.finish)
+                    )
+                    , '-1')
+                )
             }
         else
             local stepNumber = tonumber(source.step[1])
             if stepNumber and stepNumber > 0 then
                 callback {
-                    start  = source.loc.value.start,
+                    start  = source.init.start,
                     finish = source.step.finish,
-                    message = lang.script('DIAG_COUNT_DOWN_LOOP', ('%s, -%s'):format(text:sub(source.loc.value.start, source.max.finish), source.step[1]))
+                    message = lang.script('DIAG_COUNT_DOWN_LOOP'
+                        , ('%s, -%s'):format(text:sub(
+                            guide.positionToOffset(state, source.init.start),
+                            guide.positionToOffset(state, source.max.finish)
+                        )
+                        , source.step[1])
+                    )
                 }
             end
         end

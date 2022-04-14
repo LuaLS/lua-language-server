30900bbcd5f8f6229c1ceb1621af377dcafa070a
CppCXY <812125110@qq.com>
2022-04-14 22:41:57 +0800

    reset

--- make/code_format.lua
+++ make/code_format.lua
@@ -23,15 +23,19 @@ lm:source_set 'code_format' {
         "CodeService/src/FormatElement/*.cpp",
         "CodeService/src/NameStyle/*.cpp"
     },
+    windows = {
+        -- 不要开哦
+        -- flasg = "/W3 /WX"
+    },
     macos = {
-        -- macosx10.12不支持完整的std filesystem，只好砍功能
-        defines = "NOT_SUPPORT_FILE_SYSTEM",
+        flags = "-Wall -Werror",
     },
     linux = {
-        defines = (function ()
+        defines = (function()
             if lm.platform == "linux-arm64" then
                 return "NOT_SUPPORT_FILE_SYSTEM"
             end
-        end)()
-    },
+        end)(),
+        flags = "-Wall -Werror"
+    }
 }

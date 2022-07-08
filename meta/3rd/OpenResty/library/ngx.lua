13921a07158a76c770d55b34f81de1408cda9fe7
fesily <fesil@foxmail.com>
2022-07-08 15:32:44 +0800

    meta: openresty add api run_worker_thread

--- meta/3rd/OpenResty/library/ngx.lua
+++ meta/3rd/OpenResty/library/ngx.lua
@@ -4245,5 +4245,18 @@ ngx.resp = {}
 ---@return string|'"truncated"' error
 function ngx.resp.get_headers(max_headers, raw) end
 
+---@alias ngx.thread.arg boolean|number|integer|string|lightuserdata|table
+
+---@param threadpool string
+---@param module_name string
+---@param func_name string
+---@param arg1? ngx.thread.arg
+---@param arg2? ngx.thread.arg
+---@param ... ngx.thread.arg
+---@return boolean ok
+---@return ngx.thread.arg? result_or_error
+---@return ...
+function ngx.run_worker_thread(threadpool, module_name, func_name, arg1, arg2, ...)
+end
 
 return ngx

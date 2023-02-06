95e3888ea49effefadd473d071b429a52975f688
最萌小汐 <sumneko@hotmail.com>
2023-02-06 14:15:30 +0800

    update API `setConfig`, `getConfig`

--- script/core/command/getConfig.lua
+++ script/core/command/getConfig.lua
@@ -1,5 +1,10 @@
 local config = require 'config'
+local ws     = require 'workspace'
 
+---@async
 return function (data)
-    return config.get(data[1].uri, data[1].key)
+    local uri = data[1].uri
+    local key = data[1].key
+    ws.awaitReady(uri)
+    return config.get(uri, key)
 end
--- script/core/command/setConfig.lua
+++ script/core/command/setConfig.lua
@@ -1,5 +1,11 @@
 local client = require 'client'
+local ws     = require 'workspace'
 
-return function (data)
-    client.setConfig(data)
+---@async
+---@param changes config.change[]
+return function (changes)
+    for _, change in ipairs(changes) do
+        ws.awaitReady(change.uri)
+    end
+    client.setConfig(changes)
 end

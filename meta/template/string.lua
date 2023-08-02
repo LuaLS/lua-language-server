e98b56ffbcb49b465abd2ba1eddfc73ce8f2a3c5
最萌小汐 <sumneko@hotmail.com>
2023-08-02 18:34:23 +0800

    fix template

--- meta/template/string.lua
+++ meta/template/string.lua
@@ -100,7 +100,7 @@ function string.match(s, pattern, init) end
 ---#DES 'string.pack'
 ---@param fmt string
 ---@param v1  string|number
----@param v2  string|number
+---@param v2? string|number
 ---@param ... string|number
 ---@return string binary
 ---@nodiscard

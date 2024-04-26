84c18c3063d6913122824ffbde2028f1b293a68f
actboy168 <actboy168@gmail.com>
2024-04-26 16:22:22 +0800

    update bee

--- make/modules.cpp
+++ make/modules.cpp
@@ -1,4 +1,4 @@
-#include <binding/binding.h>
+#include <bee/lua/module.h>
 
 extern "C" int luaopen_lpeglabel (lua_State *L);
 static ::bee::lua::callfunc _init(::bee::lua::register_module, "lpeglabel", luaopen_lpeglabel);

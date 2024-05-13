fe512bae4622936b02445cab713b1a779cb89b83
最萌小汐 <sumneko@hotmail.com>
2024-05-13 18:37:40 +0800

    加点日志

--- script/ltask2/service/main.lua
+++ script/ltask2/service/main.lua
@@ -15,5 +15,13 @@ if LOGLEVEL then
     log.level = tostring(LOGLEVEL):lower()
 end
 
+local version = require 'version'
+
+log.info('Lua Lsp master startup, root: ', ROOT)
+log.info('ROOT:', ROOT:string())
+log.info('LOGPATH:', LOGPATH)
+log.info('METAPATH:', METAPATH)
+log.info('VERSION:', version.getVersion())
+
 local _, service = xpcall(require, log.error, 'service')
 service.start()

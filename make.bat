aa69591ec45f20e32e42c0876dfccf5f3e2df5f0
最萌小汐 <sumneko@hotmail.com>
2024-05-14 17:31:14 +0800

    用自己的luamake

--- make.bat
+++ make.bat
@@ -2,4 +2,8 @@ git submodule update --init --recursive
 cd 3rd\luamake
 call compile\install.bat
 cd ..\..
-call 3rd\luamake\luamake.exe rebuild
+IF "%~1"=="" (
+    call 3rd\luamake\luamake.exe rebuild
+) ELSE (
+    call 3rd\luamake\luamake.exe rebuild --platform %1
+)
--- make.sh
+++ make.sh
@@ -4,4 +4,8 @@ git submodule update --init --recursive
 pushd 3rd/luamake
 ./compile/build.sh
 popd
-./3rd/luamake/luamake rebuild
+if [ -z "$1" ]; then
+    3rd/luamake/luamake.exe rebuild
+else
+    3rd/luamake/luamake.exe rebuild --platform "$1"
+fi

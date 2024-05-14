1eddb463ecf6e8b716230f599960c22384756cb5
最萌小汐 <sumneko@hotmail.com>
2024-05-14 17:59:49 +0800

    修正linux扩展名

--- make.sh
+++ make.sh
@@ -6,7 +6,7 @@ pushd 3rd/luamake
 ./compile/build.sh
 popd
 if [ -z "$1" ]; then
-    3rd/luamake/luamake.exe rebuild
+    3rd/luamake/luamake rebuild
 else
-    3rd/luamake/luamake.exe rebuild --platform "$1"
+    3rd/luamake/luamake rebuild --platform "$1"
 fi

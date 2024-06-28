6b1c8b6f3f31feac145833d29ac53b3bab7b5ad5
最萌小汐 <sumneko@hotmail.com>
2024-06-29 00:58:13 +0800

    更新class

--- script/tools/class.lua
+++ script/tools/class.lua
@@ -17,12 +17,16 @@ M._errorHandler = error
 ---@field public  __del?   fun(self: any)
 ---@field public  __alloc? fun(self: any)
 ---@field package __call   fun(self: any, ...)
+---@field package __name string
 ---@field public  __getter table
+---@field public  __setter table
+---@field public  __super  Class.Base
 
 ---@class Class.Config
 ---@field private name         string
 ---@field package extendsMap   table<string, boolean>
 ---@field package extendsCalls Class.Extends.CallData[]
+---@field package extendsKeys  table<string, boolean>
 ---@field private superCache   table<string, fun(...)>
 ---@field package superClass?  Class.Base
 ---@field public  getter       table<any, fun(obj: any)>
@@ -38,6 +42,7 @@ function M.getConfig(name)
             extendsMap   = {},
             superCache   = {},
             extendsCalls = {},
+            extendsKeys  = {},
         }, { __index = Config })
     end
     return M._classConfig[name]
@@ -45,19 +50,23 @@ end
 
 -- 定义一个类
 ---@generic T: string
+---@generic Super: string
 ---@param name  `T`
----@param super? string
+---@param super? `Super`
+---@param superInit? fun(self: Class, super: Super, ...)
 ---@return T
 ---@return Class.Config
-function M.declare(name, super)
+function M.declare(name, super, superInit)
     local config = M.getConfig(name)
     if M._classes[name] then
         return M._classes[name], config
     end
     local class  = {}
     local getter = {}
+    local setter = {}
     class.__name   = name
     class.__getter = getter
+    class.__setter = setter
 
     ---@param self any
     ---@param k any
@@ -69,15 +78,31 @@ function M.declare(name, super)
             if f then
                 local res, needCache = f(self)
                 if needCache then
-                    self[k] = res
+                    rawset(self, k, res)
                 end
                 return res
             else
                 return nil
             end
         else
-            self[k] = r
+            rawset(self, k, r)
             return r
+        end
+    end
+
+    ---@param self any
+    ---@param k any
+    ---@param v any
+    ---@return any
+    local function setterFunc(self, k, v)
+        local f = setter[k]
+        if f then
+            local res = f(self, v)
+            if res ~= nil then
+                rawset(self, k, res)
+            end
+        else
+            rawset(self, k, v)
         end
     end
 
@@ -91,6 +116,16 @@ function M.declare(name, super)
         end
     end
 
+    function class:__newindex(k, v)
+        if next(class.__setter) then
+            class.__newindex = setterFunc
+            setterFunc(self, k, v)
+        else
+            class.__newindex = nil
+            rawset(self, k, v)
+        end
+    end
+
     function class:__call(...)
         M.runInit(self, name, ...)
         return self
@@ -101,6 +136,7 @@ function M.declare(name, super)
     local mt = {
         __call = function (self, ...)
             if not self.__alloc then
+                error(('class %q can not be instantiated'):format(name))
                 return self
             end
             return self:__alloc(...)
@@ -114,8 +150,13 @@ function M.declare(name, super)
             M._errorHandler(('class %q can not inherit itself'):format(name))
         end
 
+        class.__super = superClass
         config.superClass = superClass
-        config:extends(super, function () end)
+        if superInit then
+            config:extends(super, superInit)
+        else
+            config:extends(super, function () end)
+        end
     end
 
     return class, config
@@ -159,16 +200,19 @@ function M.delete(obj)
     obj.__deleted__ = true
     local name = obj.__class__
     if not name then
-        M._errorHandler('can not delete undeclared class')
+        M._errorHandler('can not delete undeclared class : ' .. tostring(obj))
     end
 
     M.runDel(obj, name)
 end
 
 -- 获取类的名称
----@param obj table
+---@param obj any
 ---@return string?
 function M.type(obj)
+    if type(obj) ~= 'table' then
+        return nil
+    end
     return obj.__class__
 end
 
@@ -180,6 +224,9 @@ function M.isValid(obj)
        and not obj.__deleted__
 end
 
+--推荐使用“扩展语义”而不是“继承”语义 。
+--因此不适合使用`super`了。
+---@deprecated
 ---@param name string
 ---@return fun(...)
 function M.super(name)
@@ -187,7 +234,7 @@ function M.super(name)
     return config:super(name)
 end
 
----@alias Class.Extends.CallData { name: string, init?: fun(self: any, super: fun(...), ...) }
+---@alias Class.Extends.CallData { name: string, init?: fun(self: any, super: (fun(...): Class.Base), ...) }
 
 ---@generic Class: string
 ---@generic Extends: string
@@ -210,8 +257,13 @@ function M.runInit(obj, name, ...)
     end
     if not data.initCalls then
         local initCalls = {}
+        local collected = {}
 
         local function collectInitCalls(cname)
+            if collected[cname] then
+                error(('class %q has circular inheritance'):format(cname))
+            end
+            collected[cname] = true
             local class = M._classes[cname]
             local cdata  = M.getConfig(cname)
             local extendsCalls = cdata.extendsCalls
@@ -219,8 +271,13 @@ function M.runInit(obj, name, ...)
                 for _, call in ipairs(extendsCalls) do
                     if call.init then
                         initCalls[#initCalls+1] = function (cobj, ...)
+                            local firstCall = true
                             call.init(cobj, function (...)
-                                M.runInit(cobj, call.name, ...)
+                                if firstCall then
+                                    firstCall = false
+                                    M.runInit(cobj, call.name, ...)
+                                end
+                                return M._classes[call.name]
                             end, ...)
                         end
                     else
@@ -306,23 +363,50 @@ function Config:extends(extendsName, init)
     if type(init) ~= 'nil' and type(init) ~= 'function' then
         M._errorHandler(('init must be nil or function'))
     end
-    if not self.extendsMap[extendsName] then
-        self.extendsMap[extendsName] = true
+    self.extendsMap[extendsName] = true
+
+    do --复制父类的字段与 getter 和 setter
         for k, v in pairs(extends) do
-            if not class[k] and not k:match '^__' then
+            if (not class[k] or self.extendsKeys[k])
+            and not k:match '^__' then
+                self.extendsKeys[k] = true
                 class[k] = v
             end
         end
         for k, v in pairs(extends.__getter) do
-            if not class.__getter[k] then
+            if not class.__getter[k]
+            or self.extendsKeys[k] then
+                self.extendsKeys[k] = true
                 class.__getter[k] = v
             end
         end
+        for k, v in pairs(extends.__setter) do
+            if not class.__setter[k]
+            or self.extendsKeys[k] then
+                self.extendsKeys[k] = true
+                class.__setter[k] = v
+            end
+        end
     end
-    table.insert(self.extendsCalls, {
-        init = init,
-        name = extendsName,
-    })
+
+    do --记录父类的init方法
+        local rewrite
+        for i = 1, #self.extendsCalls do
+            local call = self.extendsCalls[i]
+            if call.name == extendsName then
+                call.init = init
+                rewrite = true
+                break
+            end
+        end
+        if not rewrite then
+            table.insert(self.extendsCalls, {
+                init = init,
+                name = extendsName,
+            })
+        end
+    end
+
     -- 检查是否需要显性初始化
     if not init then
         if not extends.__init then
@@ -334,6 +418,46 @@ function Config:extends(extendsName, init)
         end
         M._errorHandler(('must call super for extends "%s"'):format(extendsName))
     end
+end
+
+local isInstanceMap = setmetatable({}, { __index = function (isInstanceMap, myName)
+    local map = {
+        [myName] = true,
+    }
+    isInstanceMap[myName] = map
+
+    local config = M.getConfig(myName)
+    setmetatable(map, { __index = function (_, targetName)
+        local superName = config.superClass and config.superClass.__name
+        if superName then
+            if isInstanceMap[superName][targetName] then
+                map[targetName] = true
+                return true
+            end
+        end
+        for parentName in pairs(config.extendsMap) do
+            if isInstanceMap[parentName][targetName] then
+                map[targetName] = true
+                return true
+            end
+        end
+        map[targetName] = false
+        return false
+    end })
+    return map
+end })
+
+---检查一个对象是否是某个类的实例
+---@param obj any
+---@param targetName string
+---@return boolean
+function M.isInstanceOf(obj, targetName)
+    local myName = M.type(obj)
+    if not myName then
+        return false
+    end
+
+    return isInstanceMap[myName][targetName]
 end
 
 return M

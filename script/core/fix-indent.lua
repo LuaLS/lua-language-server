a72aa21a2f1bdaccabc757b0c8ecef17959785fd
最萌小汐 <sumneko@hotmail.com>
2024-08-01 16:11:44 +0800

    删除多余空格

--- script/core/fix-indent.lua
+++ script/core/fix-indent.lua
@@ -4,95 +4,44 @@ local lookBackward = require 'core.look-backward'
 local proto = require 'proto.proto'
 
 ---@param state parser.state
-local function insertIndentation(state, position, edits)
-    local text   = state.originText or state.lua
-    local lines  = state.originLines or state.lines
-    local row    = guide.rowColOf(position)
-    if not lines or not text then
-        return
-    end
-    local offset = lines[row]
-    local indent = text:match('^%s*', offset)
-    for _, edit in ipairs(edits) do
-        edit.text = edit.text:gsub('\n', '\n' .. indent)
-    end
-end
-
----@param state parser.state
-local function findForward(state, position, ...)
-    local lines = state.originLines or state.lines
-    local offset      = guide.positionToOffsetByLines(lines, position)
-    local firstOffset = state.originText:match('^[ \t]*()', offset + 1)
-    if not firstOffset then
-        return nil
-    end
-    for _, symbol in ipairs { ... } do
-        if state.originText:sub(firstOffset, firstOffset + #symbol - 1) == symbol then
-            return guide.offsetToPositionByLines(lines, firstOffset - 1), symbol
-        end
-    end
-    return nil
-end
-
----@param state parser.state
-local function findBackward(state, position, ...)
-    local text       = state.originText or state.lua
-    local lines      = state.originLines or state.lines
-    if not text or not lines then
-        return nil
-    end
-    local offset     = guide.positionToOffsetByLines(lines, position)
-    local lastOffset = lookBackward.findAnyOffset(text, offset)
-    if not lastOffset then
-        return nil
-    end
-    for _, symbol in ipairs { ... } do
-        if text:sub(lastOffset - #symbol + 1, lastOffset) == symbol then
-            return guide.offsetToPositionByLines(lines, lastOffset)
-        end
-    end
-    return nil
-end
-
----@param state parser.state
 ---@param change table
----@param result any[]
-local function checkSplitOneLine(state, change, result)
-    if  change.text ~= '\r\n'
-    and change.text ~= '\n' then
+---@param edits table[]
+local function removeSpacesAfterEnter(state, change, edits)
+    if not change.text:match '^\r?\n[\t ]+\r?\n' then
         return
     end
-
     local lines = state.originLines or state.lines
-    local position = lines[change.range.start.line + 1]
+    local text  = state.originText  or state.lua
+    ---@cast text -?
 
-    local fPosition, fSymbol = findForward(state, position, 'end', '}')
-    if not fPosition or not fSymbol then
-        return
+    -- 清除前置空格
+    local startPos = guide.positionOf(change.range.start.line, change.range.start.character)
+    local startOffset = guide.positionToOffsetByLines(lines, startPos)
+    local leftOffset
+    for offset = startOffset, lines[change.range.start.line], -1 do
+        leftOffset = offset
+        local char = text:sub(offset, offset)
+        if char ~= ' ' and char ~= '\t' then
+            break
+        end
     end
-    local bPosition = findBackward(state, position, 'then', 'do', ')', '{')
-    if not bPosition then
-        return
+    if leftOffset and leftOffset < startOffset then
+        edits[#edits+1] = {
+            start  = leftOffset,
+            finish = startOffset,
+            text   = '',
+        }
     end
-    local edits = {}
-    edits[#edits+1] = {
-        start  = bPosition,
-        finish = position,
-        text   = '\n\t',
-    }
-    edits[#edits+1] = {
-        start  = position,
-        finish = fPosition + 1,
-        text   = '',
-    }
-    edits[#edits+1] = {
-        start  = fPosition + 1,
-        finish = fPosition + 1,
-        text   = '\n' .. fSymbol:sub(1, 1)
-    }
-    insertIndentation(state, bPosition, edits)
-    for _, edit in ipairs(edits) do
-        result[#result+1] = edit
+
+    -- 清除后置空格
+    local endOffset = startOffset + #change.text
+    local _, rightOffset = text:find('^[\t ]+', endOffset + 1)
+    if rightOffset then
+        edits[#edits+1] = {
+            start  = endOffset,
+            finish = rightOffset,
+            text   = '',
+        }
     end
 end
 
@@ -129,41 +78,22 @@ local function applyEdits(state, edits)
         label = 'Fix Indent',
         edit = {
             changes = {
-                [state.uri] = {
-                    {
-                        range = {
-                            start = {
-                                line = 1,
-                                character = 0,
-                            },
-                            ['end'] = {
-                                line = 1,
-                                character = 0,
-                            }
-                        },
-                        newText = '\t',
-                    },
-                }
+                [state.uri] = results
             }
         },
-    })
-    proto.notify('$/command', {
-        command = 'cursorMove',
     })
 end
 
 return function (uri, changes)
-    do return end
     local state = files.compileState(uri)
     if not state then
         return
     end
 
     local edits = {}
-    for _, change in ipairs(changes) do
-        if change.range then
-            checkSplitOneLine(state, change, edits)
-        end
+    local firstChange = changes[1]
+    if firstChange.range then
+        removeSpacesAfterEnter(state, firstChange, edits)
     end
 
     applyEdits(state, edits)

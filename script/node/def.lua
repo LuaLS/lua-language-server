a7325f712f00dcd1d0dc314b021b9d5af3029a3b
最萌小汐 <sumneko@hotmail.com>
2024-09-09 18:37:00 +0800

    不应该做缓存了

--- script/node/def.lua
+++ script/node/def.lua
@@ -48,18 +48,8 @@ function M:view()
     return self.name
 end
 
----@type { [string]: Node.Def }
-ls.node.DEF = setmetatable({}, {
-    __mode = 'v',
-    __index = function (t, k)
-        local v = New 'Node.Def' (k)
-        t[k] = v
-        return v
-    end,
-})
-
 ---@param name string
 ---@return Node.Def
 function ls.node.def(name)
-    return ls.node.DEF[name]
+    return New 'Node.Def' (name)
 end

90ee7349f93fb5257681f62be17985e165a51357
最萌小汐 <sumneko@hotmail.com>
2025-03-11 17:04:10 +0800

    修正警告

--- script/vm/parser/literal.lua
+++ script/vm/parser/literal.lua
@@ -1,16 +1,20 @@
 ls.vm.registerRunnerParser('number', function (runner, source)
+    ---@cast source LuaParser.Node.Number
     return runner.node.value(source.value)
 end)
 
 ls.vm.registerRunnerParser('integer', function (runner, source)
+    ---@cast source LuaParser.Node.Integer
     return runner.node.value(source.value)
 end)
 
 ls.vm.registerRunnerParser('string', function (runner, source)
+    ---@cast source LuaParser.Node.String
     return runner.node.value(source.value)
 end)
 
 ls.vm.registerRunnerParser('boolean', function (runner, source)
+    ---@cast source LuaParser.Node.Boolean
     return runner.node.value(source.value)
 end)
 

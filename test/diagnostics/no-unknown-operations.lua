67bca187a76f44ae536b872ec928ec9f3977de04
Nathan <47441022+NathanSnail@users.noreply.github.com>
2025-12-03 22:49:31 +1300

    test(diagnostics): add test suite for no-unknown-operations operator usage

--- test/diagnostics/no-unknown-operations.lua
+++ test/diagnostics/no-unknown-operations.lua
@@ -50,3 +50,31 @@ TEST [[
 TEST [[
 (function() end)()
 ]]
+
+TEST [[
+local _ = 1 + 2
+local _ = <!"1" + "2"!>
+]]
+
+TEST [[
+local _ = "a" .. "b"
+---@class Vec2
+
+---@type Vec2, Vec2
+local a, b = {}, {}
+local _ = <!a .. b!>
+]]
+
+TEST [[
+---@type unknown
+local a
+local b = a + 2
+]]
+
+TEST [[
+---@param name string
+---@return string
+local function greet(name)
+    return "Hello " .. <!name "!"!>
+end
+]]

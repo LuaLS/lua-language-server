f0c25d69f45ef0dbd23f4385a9d6ad5ad40664bb
最萌小汐 <sumneko@hotmail.com>
2026-03-23 11:30:59 +0800

    整理代码

--- script/feature/completion/word.lua
+++ script/feature/completion/word.lua
@@ -173,40 +173,6 @@ ls.feature.provider.completion(function (param, action)
         return
     end
 
-    local word = util.getCompletionWord(param)
-    if word == '' then
-        return
-    end
-
-    if param.scope.config:get(param.uri, 'Lua.completion.showWord') ~= 'Enable' then
-        return
-    end
-    if not param.scope.config:get(param.uri, 'Lua.completion.workspaceWord') then
-        return
-    end
-
-    if not util.isStatementPosition(param) then
-        return
-    end
-
-    local words = param.scope.wordIndex:match(word)
-    for _, name in ipairs(words) do
-        if action.hasWord(name) then
-            goto continue
-        end
-        action.push {
-            label = name,
-            kind = ls.spec.CompletionItemKind.Text,
-        }
-        ::continue::
-    end
-end, -1000)
-
-ls.feature.provider.completion(function (param, action)
-    if param.inComment then
-        return
-    end
-
     local source = param.sources[1]
     local word = util.getCompletionWord(param)
 

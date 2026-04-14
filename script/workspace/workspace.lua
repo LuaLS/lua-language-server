ee5d128d80cd988160827a086be9d1324a55095d
最萌小汐 <sumneko@hotmail.com>
2026-04-14 21:48:52 +0800

    修复一个警告

--- script/workspace/workspace.lua
+++ script/workspace/workspace.lua
@@ -175,17 +175,16 @@ function m.getNativeMatcher(scp)
             end
         end
     end
-    for _, pathVal in ipairs(config.get(scp.uri, 'Lua.workspace.library')) do
-        local path = pathVal
+    for _, path in ipairs(config.get(scp.uri, 'Lua.workspace.library')) do
         if not addonRepositoryPathUpdated then
             addonRepositoryPathUpdated = true
             local addonRepositoryPath = config.get(scp.uri, 'Lua.addonRepositoryPath')
             files.updateAddonsPath(addonRepositoryPath)
         end
-        path = m.getAbsolutePath(scp.uri, path)
-        if path then
-            log.debug('Ignore by library:', path)
-            debug[#pattern+1] = path
+        local apath = m.getAbsolutePath(scp.uri, path)
+        if apath then
+            log.debug('Ignore by library:', apath)
+            debug[#pattern+1] = apath
         end
     end
     for _, path in ipairs(config.get(scp.uri, 'Lua.workspace.ignoreDir')) do

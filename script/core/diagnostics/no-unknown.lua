e041833abcf4e36fa39bbdadf0b9c8f23d259f80
最萌小汐 <sumneko@hotmail.com>
2026-04-14 22:32:15 +0800

    Revert "Reapply "Fix type inference for and/or idioms and circular dependency in tracer""
    This reverts commit 1dd194da537df432f97295ea167dfef19acd7a4e.

--- script/core/diagnostics/no-unknown.lua
+++ script/core/diagnostics/no-unknown.lua
@@ -26,29 +26,11 @@ return function (uri, callback)
     guide.eachSourceTypes(ast.ast, types, function (source)
         await.delay()
         if vm.getInfer(source):view(uri) == 'unknown' then
-            -- When a node only contains a 'variable' object whose base
-            -- declaration has a known type, this is a false positive caused
-            -- by circular dependency during compilation, not a true unknown.
-            local dominated = false
-            local node = vm.getNode(source)
-            if node then
-                for n in node:eachObject() do
-                    if n.type == 'variable' and n.base and n.base.value then
-                        local baseView = vm.getInfer(n.base):view(uri)
-                        if baseView ~= 'unknown' then
-                            dominated = true
-                            break
-                        end
-                    end
-                end
-            end
-            if not dominated then
-                callback {
-                    start   = source.start,
-                    finish  = source.finish,
-                    message = lang.script('DIAG_UNKNOWN'),
-                }
-            end
+            callback {
+                start   = source.start,
+                finish  = source.finish,
+                message = lang.script('DIAG_UNKNOWN'),
+            }
         end
     end)
 end
